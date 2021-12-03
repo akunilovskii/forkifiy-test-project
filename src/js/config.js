f9c7a8e380182df16c0e2930fdb3f550763ae84a
Aleksandr <aleksandr.kunilovskii@gmail.com>
2021-12-03 16:13:19 -0500

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 5;
+export const RES_PER_PAGE = 10;
+export const KEY = '6481ed3f-03bb-4ef4-9ca9-5a4c6477c69b';
+export const MODAL_CLOSE_SEC = 2.5;
