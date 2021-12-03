9ff782218d253c2d2964d95c9cbd838bf83a44c3
Aleksandr <17989652+akunilovskii@users.noreply.github.com>
2021-12-03 18:44:34 -0500

    Changed API key

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 5;
 export const RES_PER_PAGE = 10;
-export const KEY = '6481ed3f-03bb-4ef4-9ca9-5a4c6477c69b';
+export const KEY = '3db0d568-94eb-4399-b568-b1f59e6b8f61';
 export const MODAL_CLOSE_SEC = 2.5;
